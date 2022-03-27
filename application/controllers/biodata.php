5338da13b59544508b8cdd0ab4dea0207561e0fd
dodi56 <dodisuhendra1956@gmail.com>
2022-03-27 21:14:16 +0700

    biodata

---
+++ application/controllers/biodata.php
@@ -0,0 +1,12 @@
+<?php
+class biodata extends CI_Controller 
+{   
+    public function index()
+	{
+		echo"<h1> Hayy, salam kenal</h1>";
+        echo" Nama Saya Dodi Suhendra";
+        echo "Umur Saya 24 Tahun";
+        echo"Saya tinggal dibekasi kota";
+        
+	}
+}
